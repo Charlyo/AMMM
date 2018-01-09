4bb653b966666f94c1632126677bd101fd3ef76b
Carles Garriga <carles.garriga@est.fib.upc.edu>
2018-01-09 11:40:15 +0100

    added instance

--- BRKGA/DATA_DUMMY.py
+++ BRKGA/DATA_DUMMY.py
@@ -1,5 +1,5 @@
 '''
-data = {
+data= {
     'hours': 24,
     'numNurses': 20,
     'minHours': 4,
@@ -8,7 +8,7 @@ data = {
     'maxPresence': 14,
     'demand': [2, 2, 1, 1, 1, 2, 2, 3, 4, 6, 6, 7, 5, 8, 8, 7, 6, 6, 4, 3, 4, 3, 3, 3]
 }
-'''
+
 data = {
     'hours': 24,
     'numNurses': 20,
@@ -17,4 +17,15 @@ data = {
     'maxConsec': 3,
     'maxPresence': 14,
     'demand': [2, 2, 1, 1, 1, 2, 2, 3, 4, 6, 6, 7, 5, 8, 8, 7, 6, 6, 4, 3, 4, 3, 3, 3]
+}
+'''
+
+data = {
+    'numNurses': 300,
+    'hours': 24,
+    'demand': [136, 66, 112, 131, 84, 51, 143, 137, 36, 37, 49, 145, 56, 81, 134, 14, 20, 150, 137, 11, 35, 47, 67, 125],
+    'minHours': 1,
+    'maxHours': 10,
+    'maxConsec': 4,
+    'maxPresence': 14
 }
